7d46e0e515aad3391a1bef10cfc0fdc0d30d0c78
StephaneSando <stephanesando@gmail.com>
2025-06-19 02:08:03 +0200

    Update pipeline_meteo.py

--- pipeline_meteo.py
+++ pipeline_meteo.py
@@ -70,7 +70,7 @@ for v in villes:
 
     url = f"https://api.open-meteo.com/v1/forecast?latitude={lat}&longitude={lon}&current_weather=true"
 
-    response = requests.get(url)
+    response = requests.get(url, timeout=10)
 
     if response.status_code == 200:
         data = response.json();
